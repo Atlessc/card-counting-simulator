6fdaf400ed831bc5791bed758859b782616e9b4a
Tyler S <107712050+Atlessc@users.noreply.github.com>
2024-06-06 13:46:50 -0700

    Update ZustandStore.js

--- src/ZustandStore.js
+++ src/ZustandStore.js
@@ -24,7 +24,7 @@ const shuffleDeck = (deck) => {
   for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[j]] = [deck[j], deck[i]];
-    console.log(deck)
+    console.log(`${deck}`)
   }
   return deck;
 };
