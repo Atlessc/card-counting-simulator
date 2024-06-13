0d91fe79867043a94b2ba4b1bd799a2bd99911d3
Tyler Smith <tylersmith4596@gmail.com>
2024-06-13 00:58:24 -0700

    chore: Update ZustandStore.js and npm dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
-// import useStore from './ZustandStore.js';
+import useStore from './ZustandStore.js';
 import Practice from "./pages/PracticePage/PracticePage.jsx";
 import NavBar from "./components/NavBar/NavBar";
 // import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react";
-import  useStore from "./ZustandStore";
 import CookieConsentStatus from "./enums/CookieConsentEnums.js";
 import CookieNotice from "./components/CookieConsent/CookieConsent.jsx";
 import PrivacyPolicy from "./pages/Privacy Policy/PrivacyPolicy.jsx";
