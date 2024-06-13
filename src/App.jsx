2c33fd880513d0450ac8044eda4292d43696655e
Tyler Smith <tylersmith4596@gmail.com>
2024-06-12 23:41:08 -0700

    chore: Remove unused Footer component import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 // import useStore from './ZustandStore.js';
 import Practice from "./pages/PracticePage/PracticePage.jsx";
 import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 import  useStore from "./ZustandStore";
 import CookieConsentStatus from "./enums/CookieConsentEnums.js";
